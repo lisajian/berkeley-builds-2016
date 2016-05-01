bbf5fa762c8976a34d7e11a8569db61381639056
harrisonkuo <harrisonkuo@outlook.com>
2016-04-30 17:21:20 -0700

    Updated gitignore. Added more dependencies. Run npm install please.

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
-var express = require('express');
+var express = require('express'),
+    fs = require('fs')
+    url = require('url');
 var app = express();
-app.use('/', express.static(__dirname + '/'));
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+
+app.configure(function(){
+  app.use('/', express.static(__dirname + '/'));  
+  app.use(express.static(__dirname + '/')); 
 });
+
+app.post('/receive', function(request, respond) {
+    var body = '';
+    filePath = __dirname + '/workshop/data.txt';
+    request.on('data', function(data) {
+        body += data;
+    });
+
+    request.on('end', function (){
+        fs.appendFile(filePath, body, function() {
+            respond.end();
+        });
+    });
+});
+
+app.listen(8080);
