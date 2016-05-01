2d7147dd3c1cd7c4f85320e059376b31aa13c9e4
Lisa Jian <lisajian@berkeley.edu>
2016-05-01 12:01:38 -0700

    modifications to story portion

--- server2.js
+++ server2.js
@@ -1,14 +1,34 @@
 $(document).ready(function() {
     var clicks = 0;
-    function clickCount() {
-        console.log(clicks);
+    var imgURL;
+
+    function EL(id) { return document.getElementById(id); }
+    function readFile() {
+        if (this.files && this.files[0]) {
+            var FR = new FileReader();
+            FR.onload = function(e) {
+                imgURL = e.target.result;
+                EL("img").src = imgURL;
+                $("#img").css("height", "100%");
+                // EL("b64").innerHTML = imgURL;
+            };
+            FR.readAsDataURL( this.files[0] );
+        }
     }
+
+    EL("picture").addEventListener("change", readFile, false);
+
     $('input#submit').click(function() {
         var d = new Date();
         currentDate = d.toDateString();
         var title = $('#title').val();
         var text = $('textarea#story').val();
         var file = $('#picture').val();
+        if (file) {
+            file = imgURL;
+        } else {
+            file = "";
+        }
         obj = {
             title: title,
             text: text,
@@ -17,11 +37,13 @@ $(document).ready(function() {
         };
         var info = JSON.stringify(obj);
         if (title && (text || file)) {
-            localStorage.setItem(clicks, obj);
-            $('#result').prepend(localStorage.getItem(clicks)[2]);
+            localStorage.setItem(clicks, info);
+            $('#result').prepend(localStorage.getItem(clicks));
             $('#title').val("");
             $('textarea#story').val("");
             $('#picture').val("");
+            EL("img").src = "";
+            $('#img').css("height", "300px");
             clicks++;
         }
     });
